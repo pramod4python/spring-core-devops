7ca149076ccafc9f0d366b57504e9b6f22373a7d
pramod4python <pramodmaller@gmail.com>
2017-07-23 11:38:22 +0530

    Test CI Build 23-Jul

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringCoreDevOpsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting the Spring Boot Application..");
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 	}
 }
